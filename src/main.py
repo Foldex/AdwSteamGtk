d791458664b0c5059b286b4124db69bb02a619be
Foldex <foldex@pm.me>
2022-10-15 09:24:59 -0400

    Version Bump: 0.1.3

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ class Adwaita_steam_gtkApplication(Adw.Application):
                                 application_icon='io.github.Foldex.AdwSteamGtk',
                                 website='https://github.com/Foldex/AdwSteamGtk',
                                 issue_url='https://github.com/Foldex/AdwSteamGtk/issues/new/choose',
-                                version='0.1.2',
+                                version='0.1.3',
                                 developers=[
                                     'Foldex https://github.com/Foldex',
                                     'Christoph Kohnen https://github.com/ChaosMelone9'
