9ad626e0b4ca86131a56e1243d9974311559ffbe
Foldex <foldex@pm.me>
2023-07-30 10:22:50 -0400

    update_pot: Use Meson package version

--- po/update_pot.sh
+++ po/update_pot.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 PO_DIR=$(dirname "$(realpath "$0")")
+PROJ_DIR="$PO_DIR/.."
 OUT_FILE="$PO_DIR/AdwSteamGtk.pot"
+MESON_FILE="$PROJ_DIR/meson.build"
 
-cd "$PO_DIR/.." || exit
+PKG_VER=$(grep -oP "(?<=version: \')[0-9]+\.[0-9]+\.[0-9]+" "$MESON_FILE")
+
+if [[ ! $PKG_VER =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	echo "Invalid PKG VERSION: $PKG_VER"
+	exit 1
+else
+	echo "PKG VERSION: $PKG_VER"
+fi
+
+cd "$PROJ_DIR" || exit
 
 xgettext \
 	-f "$PO_DIR"/POTFILES \
@@ -12,7 +23,7 @@ xgettext \
 	--keyword=C_:1c,2 \
 	--keyword=_ \
 	--package-name="AdwSteamGtk" \
-	--package-version="0.6.6"
+	--package-version="$PKG_VER"
 
 sed -i \
 	-e "s/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR./Foldex, $(date +%Y)./" \
